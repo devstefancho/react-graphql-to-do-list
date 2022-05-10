b844af725e1902bcd436d72e939937350e93bdd8
fitpet <fitpet@saturnucBookPro.localdomain>
2022-05-10 12:07:44 +0900

    prisma working...

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { ApolloServer } from 'apollo-server-express';
 import { readFile } from 'fs/promises';
+import { PrismaClient } from "@prisma/client";
 import { resolvers } from "./resolvers.js";
 
+const prisma = new PrismaClient()
 const PORT = 9000;
 const app = express();
 
@@ -16,6 +18,16 @@ await server.start();
 server.applyMiddleware({
   app,
   path: '/graphql'
+})
+
+app.post('/create', async (req, res) => {
+  const init = await prisma.post.create({
+    data: {
+      title: 'cleaning room',
+      content: 'clean the room everyday'
+    }
+  })
+  res.json(init);
 })
 
 app.listen(PORT, () => {
